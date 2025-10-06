feebbf51a856c20fd29a5b9060f69f286675d4d7
Zeshan Ahmad <zahmad5870@gmail.com>
2025-10-06 10:16:58 -0400

    prototype autocorrecting at careted word works

--- src/Components/AutoCorrectTextInput.jsx
+++ src/Components/AutoCorrectTextInput.jsx
@@ -9,7 +9,7 @@ import { testAutoCorrect, readEnglishFile } from "zautocorrect";
 
 const AutoCorrectTextInput = ({ documentElementId }) => {
   const inputVal = useRef("");
-  const [correctWords, setCorrectWords] = useState([]);
+  const [correctWords, setCorrectWords] = useState();
   const wordIndex = useRef(0);
   const [dictionary, setDictionary] = useState();
 
@@ -50,7 +50,7 @@ const AutoCorrectTextInput = ({ documentElementId }) => {
    * and then we can join the array and just send that word to the
    * library autocorrect function
    */
-  const checkWordCorrectHandler = (word) => {
+  const handleWordCorrectCheck = (word) => {
     console.log("word: ", word);
 
     const currentWordsArr = word.split(" ");
@@ -81,9 +81,101 @@ const AutoCorrectTextInput = ({ documentElementId }) => {
      * somehow the previous words as well as spaces?
      */
     let wordsArr = testAutoCorrect(dictionary, wordForTestAutoCorrect);
+    //normalize it so that if it just returns the correct word, its a string, otherwise an array
+    if (wordsArr.length >= 5 && wordsArr.length <= 20) {
+      setCorrectWords(Array.isArray(wordsArr) ? wordsArr : [wordsArr]);
+    } else {
+      setCorrectWords(
+        Array.isArray(wordsArr) ? wordsArr.slice(0, 5) : [wordsArr.slice(0, 5)]
+      );
+    }
 
-    setCorrectWords([wordsArr]);
-    console.log(correctWords);
+    console.log("correct Words Array", wordsArr.slice(0, 5));
+  };
+
+  /*left off at string manipulation on the inputVal
+currently trying to turn the inputval into an array 
+also turning the word into an array
+and traversing the array, switching the elemtns at the elements
+currently not working....
+so this definitly wont work because I forgot that longer words can get 
+get switched fine but longer correction words will reach a space before
+being able to finish the correction, only replacing parcial. instead
+I should find the index at which the first space is before the word
+that needs to be corrected, and then start changing from that index 
+onwards
+*/
+  const handleInputCorrection = (word) => {
+    //fuck I gotta worry about the caret
+    const inputValArrLength = inputVal.current.value.split("");
+    let lengthToStartCorrect = 0;
+
+    for (let i = wordIndex.current - 1; i >= 0; i--) {
+      if (inputValArrLength[i] == " ") {
+        console.log("the lengthToStartCorrect: ", i);
+        lengthToStartCorrect = i;
+        break;
+      }
+      lengthToStartCorrect = i;
+    }
+    console.log(
+      "the lengthToStartCorrect after for loop: ",
+      lengthToStartCorrect
+    );
+    const inputValArr = inputVal.current.value.split("");
+    console.log("correction handler array: ", inputValArr);
+    let wordArrForInputValReplace = inputValArr;
+    const wordArr = word.split("");
+    console.log("word that replace: ", wordArr);
+    let wordCounter = 0;
+
+    for (
+      let i = lengthToStartCorrect + 1;
+      i <= lengthToStartCorrect + wordArr.length + 1;
+      i++
+    ) {
+      console.log("Right before return: ", i);
+      if (inputValArr[i] == " ") {
+        console.log("returning early");
+        break;
+      }
+      console.log(
+        "wordArrForInputValReplace before changing: ",
+        wordArrForInputValReplace
+      );
+      if (lengthToStartCorrect == 0) {
+        wordArrForInputValReplace[i - 1] = wordArr[wordCounter];
+      } else {
+        wordArrForInputValReplace[i] = wordArr[wordCounter];
+      }
+
+      wordCounter++;
+      console.log(
+        "wordArrForInputValReplace inside loop:",
+        wordArrForInputValReplace
+      );
+    }
+
+    //testing a different method
+    // for (let i = wordIndex.current - 1; i >= 0; i--) {
+    //   console.log("Right before return: ", i);
+    //   if (inputValArr[i] == " ") {
+    //     console.log("returning early");
+    //     break;
+    //   }
+    //   console.log(
+    //     "wordArrForInputValReplace before changing: ",
+    //     wordArrForInputValReplace
+    //   );
+    //   wordArrForInputValReplace[i] = wordArr[wordCounter];
+    //   wordCounter--;
+    //   console.log(
+    //     "wordArrForInputValReplace inside loop:",
+    //     wordArrForInputValReplace
+    //   );
+    // }
+    console.log("wordArrForInputValReplace: ", wordArrForInputValReplace);
+    inputVal.current.value = wordArrForInputValReplace.join("");
   };
 
   return (
@@ -95,15 +187,30 @@ const AutoCorrectTextInput = ({ documentElementId }) => {
         ref={inputVal}
         placeholder="Lets test zautocorrect"
         onChange={(e) => {
-          checkWordCorrectHandler(e.target.value);
+          handleWordCorrectCheck(e.target.value);
         }}
       />
-
-      {correctWords.map((word, idx) => (
-        <div key={idx}>
-          <li>{word}</li>
-        </div>
-      ))}
+      <div style={{ flex: "row", display: "flex" }}>
+        {correctWords?.map((word) => (
+          <div
+            key={word}
+            style={{
+              border: "1px solid blue",
+              width: "90px",
+              height: "20px",
+              alignItems: "center",
+              display: "flex",
+              margin: "3px",
+              borderRadius: "4px",
+            }}
+            onClick={() => {
+              handleInputCorrection(word);
+            }}
+          >
+            <p>{word}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
